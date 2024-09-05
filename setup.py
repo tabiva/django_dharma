bf1ff739aa701e2f83936ee93997964069cbb285
tabiva <sirio.taba85@gmail.com>
2024-09-05 22:17:22 +0200

    Adding package stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-dharma",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    description="A Django app to run checks on models",
+    author="Ivan Tabarelli",
+    author_email="itabarelli@google.com",
+    url="https://github.com/tabiva/django-dharma",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "Django>=3.2",
+    ],
+)
