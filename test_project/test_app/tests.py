2d8af4faa6063d8705252777d8da831dd01fef8b
tabiva <sirio.taba85@gmail.com>
2024-09-08 16:15:53 +0200

    Adding base check tests

--- test_project/test_app/tests.py
+++ test_project/test_app/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from test_app import models
 
-# Create your tests here.
+from django_dharma.base import count_check
+from django_dharma.models import Anomaly
+
+
+class TestAppChecksTestCase(TestCase):
+    def setUp(self):
+        models.TestModel.objects.create(field1="field1", field2="field2")
+
+    def test_count_check(self):
+        """Assert does not trigger an exception"""
+        count_check(model=models.TestModel, filters={}, count=1)
+        self.assertEqual(Anomaly.objects.count(), 0)
